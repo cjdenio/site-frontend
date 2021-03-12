50f4e906df17d4da177c79277c5266af1520eca5
Caleb Denio <cjdenio44@gmail.com>
2021-03-12 10:43:54 -0500

    ⚡️ Add display: swap to Google Fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,6 +77,7 @@ export default {
     families: {
       'Josefin+Sans': [100, 200, 300, 400, 500, 600, 700, 800, 900],
     },
+    display: 'swap',
   },
   router: {
     extendRoutes(routes, resolve) {
