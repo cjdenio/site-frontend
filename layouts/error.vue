2ea0bd5be3711abb81b60ef96853961e57d80ac6
Caleb Denio <cjdenio44@gmail.com>
2021-05-24 21:46:53 -0400

    🥅 Gadzooks!

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="page-centered">
+    <h1>ˌɡadˈzo͞oks!</h1>
+    <h2 :style="{ fontWeight: 'normal', margin: '10px 0 20px 0' }">
+      you've stumbled across a
+    </h2>
     <h1 :style="{ fontSize: '60px' }" class="fourohfour">
       <span :class="{ green: outside }">4</span>
       <span :class="{ green: !outside }">0</span>
